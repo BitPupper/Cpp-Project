ff152d6970602ffd7dadc5ef9ff5390cb94c5fca
Nono <38902034+BitPupper@users.noreply.github.com>
2019-05-16 23:18:29 +0900

    Add files via upload

--- TTT class version.cpp
+++ TTT class version.cpp
@@ -12,9 +12,8 @@ class Game
 	private://:::::Variables::::://
 		string name1, name2;
 		int row, col;
-		//int boardNum[3][3];
         char board[3][3];
-        int turn;
+        int turn, score;
 		
 	public:
 		Game()//:::::Constructor::::://
@@ -25,10 +24,10 @@ class Game
 			    for(int j=0;j<3;j++)
 			    {
 				    board[i][j]='-';
-				    //boardNum[i][j]=0;
 				}
 			turn=0;
 			row=col=-1;
+			score=0;
 		}
 		
 		//:::::Prints instructions if prompted, initializes all settings::::://
@@ -40,7 +39,6 @@ class Game
 			    for(int j=0;j<3;j++)
 			    {
 				    board[i][j]='-';
-				    //boardNum[i][j]=0;
 				}
 			turn=0;
 			row=col=-1;
@@ -54,10 +52,10 @@ class Game
 			}
 			if(ans=='y'||ans=='Y')
 			{
-				cout<<"Here are the rules:\nThe game starts with an empty 3x3 grid square, with two opposing players. User is O, the computer is X.\nPlayers take turns to place their mark on one of the empty cells.\nThe player to make 3 in a row (diagonal, vertical, norizontal) first, wins.\nThe program will ask players to input (x,y) coordinates of the slot they want to use."<<endl;
+				cout<<"Here are the rules:\nThe game starts with an empty 3x3 grid square, with two opposing players. \nPlayers take turns to place their mark on one of the empty cells.\nThe player to make 3 in a row (diagonal, vertical, norizontal) first, wins.\nThe program will ask players to input (x,y) coordinates of the slot they want to use."<<endl;
 			}
 	
-			cout<<"By default, user is O and computer is X, and user goes first."<<endl<<"Would you like to change the player names? (y/n)";
+			cout<<"By default, user is X and computer is O, and user goes first."<<endl<<"Would you like to change the player names? (y/n)";
 			cin>>ans;
 	
 			while(ans!='y'&& ans!='Y'&& ans!='N'&& ans!='n')
@@ -75,9 +73,8 @@ class Game
 			}
 
 			cout<<endl<<"You're all set!";
-			Sleep(2000);
+			Sleep(2000);//I thought too much text at once is confusing for the player, so I put some time before the game actually starts
 			cout<<endl<<endl<<endl;
-			//startGame();
 		}
 		//::::::::::::::::::::::::::::::::::::::::::::::::::::::://
         
@@ -96,7 +93,7 @@ class Game
 				if(turn!=0)
 			    	cout<<"last turn - "<<name1<<": "<<row<<","<<col<<endl;
 				break;
-			default:cout<<"Error: could not identify turn"<<endl;
+			default:cout<<"Error: could not identify turn"<<endl;//this is to make it easier for me to know where a problem is occuring
 			}
 			printBoard();
 		}
@@ -105,9 +102,10 @@ class Game
 		//*****Prints the game board for every turn*****//
 		void printBoard()
 		{
-			cout<<endl;
+			cout<<endl<<setw(22)<<" 0      1      2"<<endl;
 			for(int j=0; j<3; j++)
 			{
+				cout<<j;
 				for(int i=0; i<3; i++)
 				{
 					cout<<setw(7)<<board[i][j];
@@ -149,12 +147,10 @@ class Game
 			switch(turn%2)
 			{
 			case 0:
-				board[row][col]='O';
-				//boardNum[row][col]=1;
+				board[row][col]='X';
 				break;
 			case 1:
-				board[row][col]='X';
-				//boardNum[row][col]=-1;
+				board[row][col]='O';
 				break;
 			default:cout<<"Error: could not overwrite board."<<endl;
 			}
@@ -165,167 +161,182 @@ class Game
 		//::::::::::::Checks for wins, returns boolean value:::::::::::://
 		bool checkWin()
 		{
-			if(board[0][0]==board[1][1]&&board[1][1]==board[2][2])//diagonal, up left to bottom right
+		    for (int i=0; i<3; i++)
 			{
-				if(board[0][0]=='O')
+				//Horizontal wins
+                if (board[i][0]=='X' && board[i][1]=='X' && board[i][2]=='X')
+                {
+                	cout<<name1<<" wins!";
+                	return true;
+                }
+                else if (board[i][0]=='O' && board[i][1]=='O' && board[i][2]=='O')
+                {
+                	cout<<name2<<" wins!";
+                	return true;
+				}
+				
+				//Vertical wins
+                else if (board[0][i]=='X' && board[1][i]=='X' && board[2][i]=='X')
+                {
+                	cout<<name1<<" wins!";
+                	return true;
+				}
+				else if (board[0][i]=='O' && board[1][i]=='O' && board[2][i]=='O')
 				{
+					cout<<name2<<" wins!";
+                	return true;
+				}
+            }
+               if(board[0][0]=='X'&&board[1][1]=='X'&&board[2][2]=='X')//diagonal, up left to bottom right
+			    {
 					cout<<name1<<" wins!";
 					return true;
 					
 				}
-				else if(board[0][0]=='X')
-				{
+				else if(board[0][0]=='O'&&board[1][1]=='O'&&board[2][2]=='O')//diagonal, up left to bottom right
+			    {
 					cout<<name2<<" wins!";
-					return true;
-				}
-				else
-				{
-					return false;
-				}
-			}
-			else if(board[0][2]==board[1][1]&&board[1][1]==board[2][0])//diagonal, up right to bottom left
-			{
-					if(board[0][2]=='O')
-				{
-					cout<<name1<<" wins!";
 					return true;
 					
 				}
-				else if(board[0][2]=='X')
-				{
-					cout<<name2<<" wins!";
+			    else if(board[0][2]=='X'&&board[1][1]=='X'&&board[2][0]=='X')//diagonal, up right to bottom left
+			    {
+			    	cout<<name1<<" wins!";
+			    	return true;
+			    }
+			    else if(board[0][2]=='O'&&board[1][1]=='O'&&board[2][0]=='O')//diagonal, up right to bottom left
+		        {
+		        	cout<<name2<<" wins!";
 					return true;
 				}
-				else
+				else if(fullboard()) //Tie
+				{
+					cout<<"Board full, no possible moves left. It's a tie!";
+					return true;
+				}
+				else //Game is continued
 				{
 					return false;
 				}
-			}
-			else if(board[0][0]==board[1][0]&&board[1][0]==board[2][0])//vertical, left column
+		}
+		//::::::::::::::::::::::::::::::::::::::::::::::::::::::://
+		
+		//::::::::::::CPU, calls calc():::::::::::://
+		void AI() // "AI"
+		{
+			//If an almost-win is not detected, put mark in random slot
+			if(!calc())
 			{
-					if(board[0][0]=='O')
-				{
-					cout<<name1<<" wins!";
-					return true;
-					
-				}
-				else if(board[0][0]=='X')
-				{
-					cout<<name2<<" wins!";
-					return true;
-				}
-				else
-				{
-					return false;
-				}
-			}
-			else if(board[0][1]==board[1][1]&&board[1][1]==board[2][1])//vertical, middle column
-			{
-					if(board[0][1]=='O')
-				{
-					cout<<name1<<" wins!";
-					return true;
-					
-				}
-				else if(board[0][1]=='X')
-				{
-					cout<<name2<<" wins!";
-					return true;
-				}
-				else
-				{
-					return false;
-				}
-			}
-			else if(board[0][2]==board[1][2]&&board[1][2]==board[2][2])//vertical, right column
-			{
-					if(board[0][2]=='O')
-				{
-					cout<<name1<<" wins!";
-					return true;
-					
-				}
-				else if(board[0][2]=='X')
-				{
-					cout<<name2<<" wins!";
-					return true;
-				}
-				else
-				{
-					return false;
-				}
-			}
-			else if(board[0][0]==board[0][1]&&board[0][1]==board[0][2])//horizontal, first row
-			{
-					if(board[0][0]=='O')
-				{
-					cout<<name1<<" wins!";
-					return true;
-					
-				}
-				else if(board[0][0]=='X')
-				{
-					cout<<name2<<" wins!";
-					return true;
-				}
-				else
-				{
-					return false;
-				}
-			}
-			else if(board[1][0]==board[1][1]&&board[1][1]==board[1][2])//horiontal, middle row
-			{
-					if(board[1][0]=='O')
-				{
-					cout<<name1<<" wins!";
-					return true;
-					
-				}
-				else if(board[1][0]=='X')
-				{
-					cout<<name2<<" wins!";
-					return true;
-				}
-				else
-				{
-					return false;
-				}
-			}
-			else if(board[2][0]==board[2][1]&&board[2][1]==board[2][2])//horizontal, bottom row
-			{
-					if(board[2][0]=='O')
-				{
-					cout<<name1<<" wins!";
-					return true;
-					
-				}
-				else if(board[2][0]=='X')
-				{
-					cout<<name2<<" wins!";
-					return true;
-				}
-				else
-				{
-					return false;
-				}
-			}
-			else if(fullboard()==true)
-			{
-				cout<<"No more slots left. It's a tie!";
-				return true;
-			}
-			else{
-				return false;
+			    srand (time(NULL));
+			    do{
+			    row=rand() % 3 ;
+			    col=rand() % 3 ;
+		        }while(board[row][col]!='-');
 			}
 		}
+		//::::::::::::::::::::::::::::::::::::::::::::::::::::::://
 		
-		void AI() // AI
+		//::::::::::::Checks almost-wins by the user, blocks it:::::::::::://
+		bool calc()
 		{
-			srand (time(NULL));
-			do{
-			row=rand() % 3 ;
-			col=rand() % 3 ;
-		    }while(board[row][col]!='-');//places mark in random slot
+			
+			for (int i=0; i<3; i++)
+			{
+				//horizontal check
+				score=0;
+				for(int j=0; j<3; j++)
+				{
+					if (board[i][j]=='X')
+					score+=10;
+				}
+				
+				if(score==20)
+			    {
+				    for(int k; k<3; k++)
+				    {
+				    	if(board[i][k]=='-')
+				    	{
+				    		row=i;
+				    		col=k;
+				    		return true;
+						}
+					}
+			    }
+			}
+			    
+			for (int i=0; i<3; i++)
+			{
+			    //vertical check
+			    score=0;
+			    for(int j=0; j<3; j++)
+				{
+					if (board[j][i]=='X')
+					score+=10;
+				}
+				
+				if(score==20)
+			    {
+				    for(int k=0; k<3; k++)
+				    {
+				    	if(board[k][i]=='-')
+				    	{
+				    		row=k;
+				    		col=i;
+				    		return true;
+						}
+					}
+			    }
+			}
+			
+			//diagonal check
+			score=0;
+			for(int x=0;x<3;x++)
+			{
+				if(board[x][x]=='-')
+				{
+					score+=10;
+				}
+			}
+			if(score==20)
+			{
+				for(int x=0;x<3;x++)
+			    {
+			    	if(board[x][x]=='-')
+			    	{
+			    		row=x;
+			    		col=x;
+			    		return true;
+					}
+				}
+				
+			}
+			
+			
+			score=0;
+			for(int x=0;x<3;x++)
+			{
+				if(board[x][2-x]=='-')
+				{
+					score+=10;
+				}
+			}
+			if(score==20)
+			{
+				for(int x=0;x<3;x++)
+			    {
+			    	if(board[x][2-x]=='-')
+			    	{
+			    		row=x;
+			    		col=2-x;
+			    		return true;
+					}
+				}
+				
+			}
+			
+			//if program could not detect an almost-win from player
+			return false;
+			
 		}
 		//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::://
 		
@@ -357,7 +368,7 @@ int main()
 		TTT.input();
 	}while(TTT.checkWin()==false&&TTT.fullboard()==false);
     TTT.printBoard();
-    cout<<endl<<"Would you like to play again? (y/n): ";
+    cout<<endl<<"Would you like to play again? (y/n): ";//allows player to do a rematch without restarting program
     cin>>ans;
     while(ans!='y'&&ans!='Y'&&ans!='n'&&ans!='N'){
     cout<<"Invalid response, please enter (y)es or (n)o: ";
